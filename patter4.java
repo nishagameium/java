31d6e0543ef9776a0de567981866c8de3c04d7aa
Nisha Jatiya <nisha.jatiya@gameium.com>
2024-01-24 17:26:10 +0530

    java

--- patter4.java
+++ patter4.java
@@ -4,7 +4,7 @@ public class patter4 {
             for(int j=1; j<=i; j++)
             {
             System.out.print("* ");
-            }
+            }System.out.println();
         }
     }
 }
--- patter_practice.java
+++ patter_practice.java
@@ -75,13 +75,26 @@ public class patter_practice {
         //     for(int j=1; j<=n-i; j++)
         //     {
         //         //System.out.print(j+" ");
-        //         System.out.print(i+" ");
+        //         System.out.print(i);
         //     }
         //     for(int j=1; j<=n-i; j++)
         //     {
         //         System.out.print(" ");
         //     }System.out.println();
         // }
+
+        int n=5;
+        for(int i=1; i<=n ; i++){
+            for(int j=1; j<=n; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n-i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
 
     }
 }
